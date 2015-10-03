50f7d5e80c3150dad11239ab885ba9e3509bc587
christin <christin.engstrom@gmail.com>
2015-10-03 13:20:56 -0700

    Latest version on matching.py - works through 1 game.

--- matching.py
+++ matching.py
@@ -23,34 +23,62 @@ babyiguana = Tile("iguana", "baby iguana", "url")
 cow = Tile("cow", "cow", "url")
 babycow = Tile("cow", "baby cow", "url")
 
-cards = [Tile('tiger', 'baby tiger', 'url'), 
-Tile('walrus', 'baby walrus', 'url'), 
-Tile('iguana', 'iguana', 'url'),
-Tile('iguana', 'baby iguana', 'url'),
-Tile('dog', 'dog', 'url'),
-Tile('dog', 'baby dog', 'url')]
+cards = [
+walrus,
+babywalrus, 
+tiger,
+babytiger,
+narwhal,
+babynarwhal,
+orangutan,
+babyorangutan,
+robin,
+babyrobin,
+dog,
+babydog,
+iguana,
+babyiguana,
+cow,
+babycow
+]
+
+# class Grid(object):
+#   def __init__(cards):
+#     self.cards = cards
+#     self.selected = []
 
 def play():
     selected = []
-while True:
-    grid = random.sample(cards, len(cards))
-   #pick card, eventually: int(raw_input("choose a number!"))
-    i = 6
-    print "You picked %s" %grid[i-1].name 
-    #pick  a second card, eventually: int(raw_input("choose another number!"))
-    k = 5
-    print "You picked %s" % grid[k-1].name
-    # k cannot == i
-    if k == i:
-      print "Sorry, you cannot guess the same tile twice."
-    if k != i and grid[i-1].kind == grid[k-1].kind:
-    # later, after you have this working add x-1 and y-1 to selected list...
-      print "it's a match!"
-    else:
-      if grid[i-1].kind != grid[k-1].kind:
-        print "Sorry, these tiles aren't a match"
-    break
+    # grid = random.sample(cards, len(cards))
+    grid = cards
+    while True:
+        while True:
+            i = int(raw_input("choose a numbered tile!"))
+            if i not in selected:
+                print "You picked %s" %grid[i-1].name
+                break
+            else:
+                print "This number in unavailable, pick again."
+                    
+        while True: 
+            k = int(raw_input("choose another numbered tile!"))
+            if k == i:
+                print "Sorry, you cannot guess the same tile twice."
+            if i not in selected:
+                print "You picked %s" %grid[k-1].name
+                break
+            else:
+                print "This number in unavailable, pick again."
 
+        if k != i and grid[i-1].kind == grid[k-1].kind:
+            selected.append(k)
+            selected.append(i)
+            print "it's a match!"
+            print selected
+        else:
+            if grid[i-1].kind != grid[k-1].kind:
+                print "Sorry, these tiles aren't a match"
+play()
 
 
 
